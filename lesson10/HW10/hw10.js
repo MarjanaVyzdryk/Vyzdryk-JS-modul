a7e66772f251f868ceabab33cd1d30c2bf8d9fa8
MarjanaVyzdryk <marjanavuzdruk@gmail.com>
2021-12-11 00:42:37 +0200

    JS modul lesson10

---
+++ lesson10/HW10/hw10.js
@@ -0,0 +1,63 @@
+// - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
+//     - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+
+// let click = document.getElementById('text');
+// let button = document.getElementById('button');
+// button.onclick = function () {
+//     click.style.display = 'none';
+// }
+
+// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати
+// інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
+
+// document.getElementById('button1').onclick = function () {
+//   let age = document.getElementById('age').value;
+//     if (!age) {
+//         alert('Ви неввели свого віку');
+//     } else if (+age < 18) {
+//         alert('Вибачте вам мало років');
+//     } else {
+//         alert('Вітаємо на нашому сайті.');
+//     }
+// }
+
+// - Создайте меню, которое раскрывается/сворачивается при клике
+
+// let menuElement = document.querySelector('.menu');
+// let titleElement = menuElement.querySelector('.title');
+//
+// titleElement.onclick = function () {
+//     menuElement.classList.toggle('hidden');
+// };
+
+// - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
+//     Вывести список комментариев в документ, каждый в своем блоке.
+//     Добавьте каждому комментарию по кнопке для сворачивания его body.
+
+// let comments = [
+//     {name: 'Олег', body: 'Вчиться'},
+//     {name: 'Андрій', body: 'Займається карате'},
+//     {name: 'Вероніка', body: 'Грає на гітарі'},
+//     {name: 'Маряна', body: 'Йди вже на роботу!'},
+// ];
+//
+// let divFather = document.createElement('div');
+// for (const item of comments) {
+//     let div = document.createElement('div');
+//     let h3 = document.createElement('h3');
+//     let p = document.createElement('p');
+//     let btn = document.createElement('button');
+//     let hr = document.createElement('hr');
+//
+//     h3.innerText = item.name;
+//     p.innerHTML = item.body;
+//     btn.innerHTML = 'Закрити';
+//
+//     btn.onclick = () => {
+//         p.classList.add('pHidden');
+//     }
+//     div.append(h3, p, btn);
+//     divFather.append(div, hr);
+// }
+// document.body.appendChild(divFather)
+
